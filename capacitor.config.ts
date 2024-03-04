4db1adc80d6bb9409092776d948a6040e5051366
WINDSON VIANA DE CARVALHO <windson@virtual.ufc.br>
2024-03-04 19:22:54 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Olá PDM 2023.2',
+  appName: 'Olá PDM 2024',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
